34a253b6b151c0e9345bd5360a74e53f36171c31
JulianGamboa <vida.de.letras@gmail.com>
2023-04-30 16:37:11 -0300

    usando o cli e vue 3

--- 2023-abril/src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import Novas3imagens from './components/Novas3imagens.vue'
-</script>
-
-<template>
-      <Novas3imagens msg="Abril 29" />
-</template>
-
--- 2023-abril/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-//
-import { memoryDB }  from './memoryDB.js'
-
-createApp(App).mount('#app')
